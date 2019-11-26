103ede3df1f8e9aef5149252cc671f193dc6a395
Dmitry Gerasimuk <hello@dmitrygerasimuk.com>
2019-11-26 05:09:28 +0300

    parse web site and output to console

--- index.js
+++ index.js
@@ -1,29 +1,134 @@
-const osmosis = require('osmosis');
-var myData;
-//setInterval(function() {
+// jshint: es6
 
+const osmosis = require('osmosis');
+
+
+const UPDATE_TIMEOUT = 5000;
+
+
+var standObject = {};
+ let myArray = [];
+let _saveID;
+let _counter=0;;
+doThePost = (array) => {
+    max=array.length-1;
+    
+    for (i=max; i>=0; i--) {
+
+    
+    console.log(`id:        ${array[i][0]}`);
+    console.log(`дата:      ${array[i][1]}`);
+    console.log(`мест:      ${array[i][2]}`);
+    console.log(`цена:      ${array[i][3]}`);
+    console.log(`картинка:  ${array[i][4]}`);
+
+    }
+    
+
+  
+   
+    
+    
+}
+ function storeValues(array) { 
+    let myID=array[0][0];
+
+    
+
+    if (_saveID === myID) {
+        
+        console.log('Nothing changed');
+
+
+    } else {
+        
+        if (_saveID) {
+            console.log('Update found, posting');
+            doThePost(array);
+        } else {
+
+        
+        console.log('ID is new, update');
+        doThePost(array);
+        
+        }
+        _saveID = myID;
+
+    }
+
+    console.log(`Last known id: ${array[0][0]}`);
+
+
+
+ }
+ 
+
+function niceParse(data) {
+
+
+    var myObject = {
+        id: data.id[1],
+        date:data.date[1],
+        seats:data.seats[1],
+        cost:data.cost[1],
+        img:data.img[1],
+
+
+    };
+    
+    myArray.push([data.id[1],data.date[1],data.seats[1],data.cost[1],data.img[1]]);
+
+
+ };
+
+
+setInterval(function() {
 osmosis
     .get('https://standupstore.ru')
     //.set({'related': ['.infiniti-contant']})
-   .paginate('.inf-next-link > a', 10)
-    //.set({'related': ['.infiniti-contant']})
+    .paginate('.inf-next-link > a', 20)
+    .find('.js-product')
+
+    .set({'id': ['a[href]@data-id'],
+          'date': ['a[href]@data-date'],
+          'seats' : ['a[href]@data-seats'],
+          'cost' : ['a[href]@data-cost'],
+          'img': ['.t-bgimg@style']})
+          
     
-    .set({'wow': ['.t-descr'], 'img': ['.t-bgimg@style']})
-    
-    
+
 
     .data(function(data) {
-        //console.log(data.a[data.a.length-1]);
-        //console.log(data.wow[data.wow.length-1]);
-   
-        //myData = data;
-       console.log(data);
+       
+       niceParse(data);
+
+      
+
+
+    }).done(function(){
+        _counter++;
+       console.log(_counter);
+        
+        //console.log(myArray);
+        
+        console.log(myArray.length);
+        
+        console.log(myArray[0][0])
+        
+        storeValues(myArray);
+
+        myArray=[];
+        
+
 
     });
+},UPDATE_TIMEOUT);
 
-
-
+ 
 /*
+
+console.log('asda');
+
 
 
 osmosis
@@ -48,5 +153,4 @@ osmosis
    */
 
 //},1000);
-
 
