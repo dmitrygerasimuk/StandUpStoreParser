8fb00fec83689925474be4f2d8d0b6dfaa4aa795
Dmitry Gerasimuk <hello@dmitrygerasimuk.com>
2019-11-26 21:19:55 +0300

    add webserver

---
+++ start.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const ip = process.env.IP || 'localhost'
+const port = process.env.PORT || 8080
+const CHANNEL = "thatisnotfunny";
+
+const url = `https://telegram.me/${CHANNEL}`
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end(`This is StandUp bot on <a href = '${url}'>${url}</a>`)
+})
+server.listen(port)
+console.log(`Server listening at http://${ip}:${port}/`)
